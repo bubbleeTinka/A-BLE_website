6aef1b68ce4c05b3a6418c39c1b95ecb80c90f2e
Martin Hanzl <martin.hanzl@edookit.com>
2020-09-06 17:10:07 +0200

    add files to .gitignore add svg macro add logo clear readme

--- www/index.php
+++ www/index.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
+define('APP_PATH', realpath(__DIR__ . '/../app'));
+
 App\Bootstrap::boot()
 	->createContainer()
 	->getByType(Nette\Application\Application::class)
